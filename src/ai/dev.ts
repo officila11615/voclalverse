ba89a8556d0880adfac138dbbd7e18c7f057272d
unknown person <usemail11615@gmail.com>
2025-08-07 04:17:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/ai/dev.ts
+++
@@ -1,5 +0,0 @@
-import { config } from 'dotenv';
-config();
-
-// Since we are not using Genkit flows anymore, this file can be left empty
-// or you can re-add Genkit flows here if you need them in the future.
--- src/ai/genkit.ts
+++
@@ -1,3 +0,0 @@
-// This file is no longer used for text generation with OpenRouter.
-// You can remove it if you are not using Genkit for any other purpose.
-// For now, I will leave it in case you want to re-integrate Genkit later.
